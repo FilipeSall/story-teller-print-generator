17b51bd051502316408909a7b9176ea0b07b6856
Filipe Salles <filipesalles69@gmail.com>
2023-11-01 22:20:58 -0300

    novas frases

--- src/components/icon_btn/goodmessagesarray.tsx
+++ src/components/icon_btn/goodmessagesarray.tsx
@@ -10,4 +10,6 @@ export  const goodVibeMessages = [
     'Que o verão no seu sorriso nunca acabe.',
     'Ela tinha aquela fé bonita de que tudo daria certo no final.',
     'A mente é tudo. Você se torna aquilo que você pensa',
+    'Você é forte e será capaz de lutar e conquistar o seu espaço.',
+    'você só precisa se manter firme e tudo dará certo.',
 ]
