d66807b26dbcbf10d2afc62e9ce133c726f06524
Filipe Salles <filipesalles69@gmail.com>
2023-11-02 23:39:09 -0300

    alterando frases

--- src/components/icon_btn/goodmessagesarray.tsx
+++ src/components/icon_btn/goodmessagesarray.tsx
@@ -7,7 +7,6 @@ export  const goodVibeMessages = [
     'Você é forte e será capaz de lutar e conquistar o seu espaço.',
     'Você só precisa se manter firme e tudo dará certo.',
     'Conheça-se e seja sua porta-voz em todos os lugares que desejar estar.',
-    'Faça sempre o seu melhor e acredite que o melhor possa ser feito.',
     'Não importa que você vá devagar, contanto que você não pare.',
     'Que todos os nossos passos nos levem para o que nos faz feliz.',
     'Abra os olhos e aventure-se nessa jornada chamada vida!',
